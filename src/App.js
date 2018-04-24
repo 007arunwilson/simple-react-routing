871501748ecf2fd0c855b53e4b1a681dd840d864
Arun Wilson <cyberinfoscripter@gmail.com>
2018-04-24 09:12:18 +0530

    Added nested routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { Route, Link, NavLink } from 'react-router-dom';
+import { Route, Link, NavLink,Switch } from 'react-router-dom';
 
 import Courses from './containers/Courses/Courses';
-import Course from './containers/Course/Course';
 import Users from './containers/Users/Users';
 
 import './App.css';
@@ -40,41 +39,40 @@ class App extends Component {
           </div>
 
           <div className="page-contents" >
-            <Route path="/users" component={Users} />
-            <Route path="/courses" exact component={Courses} />
-            <Route path="/courses/:course_id" component={Course} />
-            <Route path="/" exact render={() => {
+              <Route path="/users" component={Users} />
+              <Route path="/courses" component={Courses} />
+              <Route path="/" exact render={() => {
 
-              return (
-                <ol style={{ textAlign: 'left' }}>
-                  <li>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
-                  <li>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
-                  <li>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</li>
-                  <li>Pass the course ID to the "Course" page and output it there</li>
-                  <li>Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</li>
-                  <li>Load the "Course" component as a nested component of "Courses"</li>
-                  <li>Add a 404 error page and render it for any unknown routes</li>
-                  <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
-                </ol>
-              );
-
-
-            }} />
-            <Route path="/about" exact render={() => {
-
-              return (
-                <p style={{
-                  padding: '4px 20%',
-                  fontSize:'12px',
-                  color:'#676565'
-                  }}>
-                  <h3>What is Lorem Ipsum?</h3>
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-              );
+                return (
+                  <ol style={{ textAlign: 'left' }}>
+                    <li>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
+                    <li>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
+                    <li>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</li>
+                    <li>Pass the course ID to the "Course" page and output it there</li>
+                    <li>Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</li>
+                    <li>Load the "Course" component as a nested component of "Courses"</li>
+                    <li>Add a 404 error page and render it for any unknown routes</li>
+                    <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
+                  </ol>
+                );
 
 
-            }} />
+              }} />
+              <Route path="/about" exact render={() => {
+
+                return (
+                  <p style={{
+                    padding: '4px 20%',
+                    fontSize:'12px',
+                    color:'#676565'
+                    }}>
+                    <h3>What is Lorem Ipsum?</h3>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                  </p>
+                );
+
+
+              }} />
           </div>
 
         </section>
--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route,Switch } from 'react-router-dom';
 import Course from '../Course/Course';
 import './Courses.css';
 
@@ -12,24 +13,51 @@ class Courses extends Component {
     }
 
     courseSelectHandler(course_id) {
+        this.props.history.push(this.props.match.url + '/' + course_id);
+    }
 
-        console.log('A Course is selected : ', course_id);
-        this.props.history.push(this.props.match.url+'/'+course_id);
+    componentDidMount() {
+
+        console.log('[Courses]-componentDidMount  props : ', this.props);
+
     }
 
     render() {
 
         let choosedCourseId = this.state.choosedCourseId;
+        let courses_content_jsx = (
+            this.state.courses.map(course => {
+                return <article onClick={() => this.courseSelectHandler(course.id)} className="Course" key={course.id}>{course.title}</article>;
+            })
+        );
+
 
         return (
+
+
             <div>
                 <h1>Amazing Udemy Courses</h1>
+
                 <section className="Courses">
-                    {
-                        this.state.courses.map(course => {
-                            return <article onClick={() => this.courseSelectHandler(course.id)} className="Course" key={course.id}>{course.title}</article>;
-                        })
-                    }
+
+                    <Switch>
+                    <Route path={this.props.match.url + '/:courseid'} render={(props_) => {
+
+                        let choosedCourseId = props_.match.params.courseid;
+                        let course_data = {};
+                        let courses = { ...this.state.courses }
+
+                        const course_keys = Object.keys(courses);
+
+                        course_keys.map(_i_key => {
+                            if (courses[course_keys[_i_key]]['id'] == choosedCourseId) course_data = { ...courses[course_keys[_i_key]] }
+                        });
+
+                        return (<Course course_data={course_data} />);
+
+                    }} />
+                    <Route render={() => courses_content_jsx} />
+                    </Switch>
                 </section>
             </div>
         );
